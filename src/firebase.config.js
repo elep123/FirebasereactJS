4607af62c9776b423f6d6d6b1a9db2f58ceb96eb
elep <syihan64@gmail.com>
2021-04-19 09:07:21 +0700

    FirebaseReactJS

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+  export default {
+    apiKey: "AIzaSyDVQxDL6cN6thJLIerltCkaD1hZhFfIX9A",
+    authDomain: "fir-reactjs-a7188.firebaseapp.com",
+    projectId: "fir-reactjs-a7188",
+    storageBucket: "fir-reactjs-a7188.appspot.com",
+    messagingSenderId: "908927301026",
+    appId: "1:908927301026:web:00e060a24a05785ae4eefb",
+  };
